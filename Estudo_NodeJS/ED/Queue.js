ae635b9f434845f381183ba7e505e0f92e85ca4d
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2023-03-01 10:46:48 -0300

    Paste ED2

---
+++ Estudo_NodeJS/ED/Queue.js
@@ -0,0 +1,6 @@
+/*Fila
+*O primeiro a entrar é o primeiro a sair (FIFO)
+*Enqueue = entrar na fila
+*Dequeue = sair da fila
+*/
+
--- Estudo_NodeJS/ED/Stack.js
+++ Estudo_NodeJS/ED/Stack.js
@@ -6,7 +6,37 @@
 
 //modelagem
 class Stack{
+    //constructor = funcao que vai ser executada assim q o objeto for criado
     constructor(){
+        this.data = []
+        this.top = -1
+    }
+    push(value){
+        this.top++
+        this.data[this.top] = value
+        return this.data
+    }
 
+    pop(){
+        if(this.top < 0) return undefined
+        const poppedTop = this.data[this.top]
+        delete this.data[this.top]
+        this.top--
+        return poppedTop 
+    }
+    peek(){
+        return this.top >= 0 ? this.data[this.top] : undefined
     }
 }
+//"new Stack" istancia um novo objeto, transformando stack em um objeto
+const stack = new Stack()
+
+//adiciona dados
+stack.push('learning')
+stack.push('Data')
+stack.push('Steph')
+console.log(stack)
+console.log(stack.peek())
+stack.pop
+console.log(stack.pop())
+console.log(stack.peek())
