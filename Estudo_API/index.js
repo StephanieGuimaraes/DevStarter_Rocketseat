0baecc633561f12fe3d4c43c812239d5f9375931
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2023-03-02 17:16:22 -0300

    API_Server

---
+++ Estudo_API/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+
+const app = express()
+//o listen fica ouvindo o navegador e quando a gente entrar na porta 3000
+//fica ouvindo uma porta para poder rodar no navegador (server)
+app.listen('3000')
