61ec2c0422329bf65979bd597c62d48d67834840
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2023-03-02 23:50:46 -0300

    API_index.js

--- Estudo_API/index.js
+++ Estudo_API/index.js
@@ -4,3 +4,10 @@ const app = express()
 //o listen fica ouvindo o navegador e quando a gente entrar na porta 3000
 //fica ouvindo uma porta para poder rodar no navegador (server)
 app.listen('3000')
+console.log("Servidor Rod");
+
+//middleware
+app.use(express.json())
+
+//app.route('/').get( (req,res) => res.send("hello") )
+app.route('/').post( (req,resp) => console.log(req.body) )
