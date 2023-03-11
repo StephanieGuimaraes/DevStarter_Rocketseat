b714d24767e3f5e2f3a135a71ccb8399c79609bd
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2023-03-11 11:28:14 -0300

    EndIndex

---
+++ Estudo_API/ParamReq.js
@@ -0,0 +1,30 @@
+/* 
+* Estudo de parametros nas requisições
+* q são formas de se passar dentro do codigo informações q nao existiam antes
+*/
+
+//criacao de uma constante q vai receber o modulo express
+//o require puxa esse modulo
+//assim  o modulo do express é gurdado nessa costante
+const express = require('express')
+//A constante do express é inicializada e guardada dentro da constante app
+//Assim sempre que se deseje usar qualquer coisa do express é só usar app.e_o_metodo_desejado_do_express
+const app = express()
+//o listen fica ouvindo o navegador e quando a gente entrar na porta 3000
+//fica ouvindo uma porta para poder rodar no navegador (server)
+app.listen('3000')
+console.log("Servidor Rodando");
+
+let author = "StephCG"  //definição de uma variavel para exemplo
+
+//MIDDLEWARE (ponte entre as requisições)
+app.use(express.json())
+
+//Uma forma de passar parametros (nesse caso o nome Steph) pela requisição: http://localhost:3000/?name=Steph
+//Outra forma de se passar parametros é pelo body
+
+app.route('/').get( (req,res) => res.send(req.query.name) )
+
+app.route('/').get( (req,res) => res.send(req.body.author) )
+
+app.route('/').get( (req,res) => res.send(req.params.parametro) )
--- Estudo_API/index.js
+++ Estudo_API/index.js
@@ -1,13 +1,44 @@
-const express = require('express')
+/* 
+* Estudo e testes dos metodos HTTP e API no Backend
+*/
 
+//criacao de uma constante q vai receber o modulo express
+//o require puxa esse modulo
+//assim  o modulo do express é gurdado nessa costante
+const express = require('express')
+//A constante do express é inicializada e guardada dentro da constante app
+//Assim sempre que se deseje usar qualquer coisa do express é só usar app.e_o_metodo_desejado_do_express
 const app = express()
 //o listen fica ouvindo o navegador e quando a gente entrar na porta 3000
 //fica ouvindo uma porta para poder rodar no navegador (server)
 app.listen('3000')
-console.log("Servidor Rod");
+console.log("Servidor Rodando");
 
-//middleware
+let author = "StephCG"  //definição de uma variavel para exemplo
+
+//MIDDLEWARE (ponte entre as requisições)
+//Fala para a API q o conteudo recebido pelo post é um json
+//.use é um metodo do express para fazer esses middlewares
 app.use(express.json())
 
+//Uso do GET-----------
 //app.route('/').get( (req,res) => res.send("hello") )
-app.route('/').post( (req,resp) => console.log(req.body) )
+//app.route('/').get( (req,res) => res.send(author))
+
+//Uso do POST----------
+//app.route('/').post( (req,resp) => console.log(req.body) )
+//app.route('/').post( (req,resp) => resp.send(req.body) )
+
+/*
+//Uso do PUT-----------
+//Normalmente usado para editar informações
+app.route('/').put( (req,resp) => {
+    author = req.body.author   //Recebe uma informação para autorar o valor original da variavel
+    resp.send(author)
+} )
+*/
+
+//Uso do DELETE
+app.route('/:identificador').delete( (req,resp) => {
+    resp.send(req.params.identificador)
+})
